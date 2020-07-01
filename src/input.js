a278ff08c49e1a910f213fc4fed3053a2722d7c6
vaibhavmahindru <500067728@stu.upes.ac.in>
2020-07-02 02:46:32 +0530

    movie card done

--- src/input.js
+++ src/input.js
@@ -1,37 +1,51 @@
 import React, { Component } from "react";
+import Movies from "./movies";
 class input extends Component {
+  state = {
+    query: "",
+    movies: [],
+  };
   render() {
     const searchMovie = async (e) => {
       e.preventDefault();
-      const query = "Jurassic";
+      //const query = "Jurassic Park";
 
-      const url = `https://api.themoviedb.org/3/search/movie?api_key=de70721a82fd944404b174a37aef7df3&language=en-US&query=${query}&page=1&include_adult=false`;
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=de70721a82fd944404b174a37aef7df3&language=en-US&query=${this.state.query}&page=1&include_adult=false`;
 
       //const url2 = `http://www.omdbapi.com/?i=tt3896198&apikey=f1d1c7f9&t=${query}`;
       //Another free movie api but not as useful as the first one as it has more movies
       try {
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data);
+        console.log(data.results);
+        this.setState({ movies: data.results });
       } catch (err) {
         console.log(err);
       }
     };
+    const movie = this.state.movies.map((movie) => (
+      <Movies key={movie.id} movie={movie} />
+    ));
     return (
-      <form className="form" onSubmit={searchMovie}>
-        <input
-          className="input"
-          placeholder="Search any movie"
-          name="query"
-          type="text"
-        />
-        <button type="submit" className="button">
-          <span role="img" aria-label="search">
-            {" "}
-            🔍
-          </span>
-        </button>
-      </form>
+      <>
+        <form className="form" onSubmit={searchMovie}>
+          <input
+            className="input"
+            placeholder="Search any movie"
+            name="query"
+            type="text"
+            value={this.state.query}
+            onChange={(e) => this.setState({ query: e.target.value })}
+          />
+          <button type="submit" className="button">
+            <span role="img" aria-label="search">
+              {" "}
+              🔍
+            </span>
+          </button>
+        </form>
+        <div className="cards-list">{movie}</div>
+      </>
     );
   }
 }
---
+++ src/movies.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class movies extends Component {
+  styles = {
+    height: "250px",
+  };
+  render() {
+    return (
+      <div className="card">
+        <img
+          className="card--image"
+          src={
+            this.props.movie.poster_path === null
+              ? "/images/not-found.png"
+              : `https://image.tmdb.org/t/p/w185_and_h278_bestv2/${this.props.movie.poster_path}`
+          }
+          alt={this.props.movie.title + " poster"}
+          style={this.props.movie.poster_path === null ? this.styles : null}
+        />
+        <div className="card--content">
+          <h3 className="card--title">{this.props.movie.title}</h3>
+          <p className="release-date">
+            <small>Release Date: {this.props.movie.release_date}</small>
+          </p>
+          <p className="rating">
+            <small>Rating: {this.props.movie.vote_average}/10</small>
+          </p>
+          <p className="card--desc">{this.props.movie.overview}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default movies;
