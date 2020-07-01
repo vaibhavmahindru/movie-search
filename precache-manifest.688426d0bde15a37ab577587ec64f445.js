dcbaedc2bf8388a7517f6b2af874b764cf6683fb
vaibhavmahindru <500067728@stu.upes.ac.in>
2020-07-02 02:56:08 +0530

    Updates

---
+++ precache-manifest.688426d0bde15a37ab577587ec64f445.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c9f3c16d0d96f00cd5e80db5c70205c",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "6b432125f0c8a45f0659",
+    "url": "/movie-search/static/css/main.36bb67cf.chunk.css"
+  },
+  {
+    "revision": "75db9e778f3c32b90f0c",
+    "url": "/movie-search/static/js/2.82b9aca1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search/static/js/2.82b9aca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b432125f0c8a45f0659",
+    "url": "/movie-search/static/js/main.7092dca3.chunk.js"
+  },
+  {
+    "revision": "8f1314dfdc21c95c9344",
+    "url": "/movie-search/static/js/runtime-main.8345f5b5.js"
+  }
+]);
